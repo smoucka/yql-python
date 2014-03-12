3be1eae3a4e59ddb65904271eb91d50cbe71af27
swm <smoucka@gmail.com>
2014-03-11 21:34:25 -0500

    update

--- yql_oauthlib.py
+++ yql_oauthlib.py
@@ -1,6 +1,16 @@
+import csv
 from requests_oauthlib import OAuth1Session
 
 vals = {}	#dict of vals to be returned
 with open('auth.csv', 'rb') as f:
 	f_iter = csv.DictReader(f)
 	vals = f_iter.next()
+
+req_token_url = 'https://api.login.yahoo.com/oauth/v2/get_request_token'
+
+oauth = OAuth1Session(client_key=vals['consumer_key'], client_secret=vals['consumer_secret'])
+fetch_response = oauth.fetch_request_token(req_token_url)
+
+print fetch_response
+
+# http://requests-oauthlib.readthedocs.org/en/latest/oauth1_workflow.html
