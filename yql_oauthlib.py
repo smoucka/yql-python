f9724ab417409eec29f738e281a3a2a5aaad1b14
smoucka <smoucka@gmail.com>
2014-03-17 09:45:09 -0500

    update

--- yql_oauthlib.py
+++ yql_oauthlib.py
@@ -11,6 +11,12 @@ base_auth_url = 'https://api.login.yahoo.com/oauth/v2/request_auth'
 access_token_url = 'https://api.login.yahoo.com/oauth/v2/get_token'
 callback = 'oob'
 
+base_rest_url = 'http://query.yahooapis.com/v1/public/yql'
+
+test_url = 'http://fantasysports.yahooapis.com/fantasy/v2/leagues'
+
+test_rest_query = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20fantasysports.leagues.standings%20where%20league_key%3D'322.l.26670'&diagnostics=true"
+
 oauth_session = OAuth1Session(vals['consumer_key'], client_secret=vals['consumer_secret'], callback_uri=callback)
 fetch_response = oauth_session.fetch_request_token(req_token_url)
 
@@ -34,11 +40,23 @@ oauth_session = OAuth1Session(vals['consumer_key'],
 
 oauth_tokens = oauth_session.fetch_access_token(access_token_url)
 
-print oauth_tokens
+#print oauth_tokens
+
+res_owner_key = oauth_tokens.get('oauth_token')
+res_owner_secret  = oauth_tokens.get('oauth_token_secret')
+
+oauth_session = OAuth1Session(vals['consumer_key'],
+								client_secret=vals['consumer_secret'],
+								resource_owner_key=res_owner_key,
+								resource_owner_secret=res_owner_secret)
+
+yql = oauth_session.get(test_url)
+
+print yql
 
 #redirect_response = raw_input('Paste redirect URL here: ')
 #oauth_response = oauth_session.parse_authorization_response(redirect_response)
 
 
-
+# http://stackoverflow.com/questions/14251851/what-is-406-not-acceptable-response-in-http
 # http://requests-oauthlib.readthedocs.org/en/latest/oauth1_workflow.html
