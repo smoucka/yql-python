ffe1fd8d4519112d5aef15911c594d9714a84bc6
swm <smoucka@gmail.com>
2014-03-18 21:49:13 -0500

    update

--- yql_fantasy_query.py
+++ yql_fantasy_query.py
@@ -11,19 +11,17 @@ with open('res_auth.csv', 'rb') as a:
 	a_iter = csv.DictReader(a)
 	res_vals = a_iter.next()
 
-test_rest_query = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20fantasysports.leagues.standings%20where%20league_key%3D'322.l.26670'&diagnostics=true"
-test_url = "http://fantasysports.yahooapis.com/fantasy/v2/leagues;league_keys=322.l.26670/standings"
-
-data = {'q': "select * from fantasysports.leagues.scoreboard where league_key='322.l.26670' and week='12'",
-		'format': 'json'}
+weekly_scoreboard_url = "http://fantasysports.yahooapis.com/fantasy/v2/league/322.l.26670/scoreboard;week=1"
 
 oauth = OAuth1(vals['consumer_key'],
 				client_secret=vals['consumer_secret'],
 				resource_owner_key=res_vals['resource_owner_key'],
 				resource_owner_secret=res_vals['resource_owner_secret'])
 
-r = requests.get(url=test_url, params=data, auth=oauth)
+r = requests.get(url=weekly_scoreboard_url, auth=oauth)
+
+with open('result_test.txt', 'w') as w:
+	w.write(r.content)
 
 print 'RESULTS BELOW'
-print r
 print r.content
