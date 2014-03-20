07facac12a609d3731f4b8d08cce997eac05aea6
swm <smoucka@gmail.com>
2014-03-19 22:14:57 -0500

    update

--- yql_result_parse.py
+++ yql_result_parse.py
@@ -1,20 +1,30 @@
 from bs4 import BeautifulSoup
 
+category_lookup = {'9004003': 'FGs',
+					'5': 'FG%',
+					'9007006': 'FTs',
+					'8': 'FT%',
+					'10': '3pt',
+					'12': 'Pts',
+					'15': 'Rbs',
+					'16': 'Ast',
+					'17': 'Stl',
+					'18': 'Blk',
+					'19': 'TOs'}
+
 with open('result_test.txt', 'rb') as f:
 	xmldata = f.read()
 
 soup = BeautifulSoup(xmldata)
 
+# rename 'name' tags to not confuse .name method
 for name_tag in soup.find_all('name'):
 	name_tag.name = 'name_'
-	print name_tag
 
-#matchups = soup.league.scoreboard.matchups.find_all('team')
+teams = soup.league.scoreboard.matchups.find_all('team')
 
-#print matchups[0].name.string
-#print matchups[0].team_key.string
-'''
-for each in matchups:
-	print each.name.string, each.team_stats
-	print '-------------------------------------------'
-'''
+for each in teams:
+	print each.name_.string
+	for st in each.find_all('stat'):
+		print category_lookup[st.stat_id.string]
+		print st.value.string
